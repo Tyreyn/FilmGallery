5754c51496569c539f7d40e16ab24de54a2414e5
Tyreyn <athosikish33@gmail.com>
2024-09-16 21:31:54 +0200

    first commit

---
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		run(BackendApplication.class, args);
+	}
+
+}
