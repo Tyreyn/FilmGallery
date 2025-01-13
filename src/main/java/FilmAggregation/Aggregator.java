cb121879a3873ed4c3907db8f0f2e7f77c3fb18a
Tyreyn <athosikish33@gmail.com>
2025-01-13 17:32:10 +0100

    added frontend

--- src/main/java/FilmAggregation/Aggregator.java
+++ src/main/java/FilmAggregation/Aggregator.java
@@ -3,13 +3,19 @@ package FilmAggregation;
 import Pojos.Rating;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,19 +47,23 @@ public abstract class Aggregator {
 
     private Rating rating;
 
+    private StringBuilder aggregatorLog;
+
     public Aggregator(String title, String productionYear) {
         this.title = title;
         this.productionYear = productionYear;
     }
 
-    public Rating GetFilmRating() {
+    @SneakyThrows
+    public Rating GetFilmRating(StringBuilder aggregatorLog) {
         this.rating = new Rating();
+        this.aggregatorLog = aggregatorLog;
         this.rating.setSite(Url);
         this.rating.setUrlOrId(FindFilmUrl());
         return rating;
     }
 
-    private String FindFilmUrl() {
+    private String FindFilmUrl() throws IOException {
         String filmIdOrUrl = null;
         ChromeOptions options = new ChromeOptions();
         options.addArguments("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36");
@@ -68,11 +78,20 @@ public abstract class Aggregator {
         options.addArguments("--no-sandbox");
         options.addArguments("--ignore-certificate-errors");
         WebDriver driver = new ChromeDriver(options);
+        this.aggregatorLog.append("\nStarting new web browser");
         try {
+            this.aggregatorLog.append("\nGoing to site: \n").append(this.Url);
             driver = this.GoToFilmSite(driver);
             filmIdOrUrl = ExtractFilmId(driver.getCurrentUrl());
+            this.aggregatorLog
+                    .append("\nExtracted film ID: \n")
+                    .append(driver.getCurrentUrl())
+                    .append("\nStarting getting rating: ");
             this.rating = this.GetRating(driver, this.rating);
-        } catch (IOException e) {
+        } catch (Exception e) {
+            this.aggregatorLog.append("\nSomething went wrong! \n").append(e.toString());
+            File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshotFile, new File("SoftwareTestingMaterial.png"));
             e.printStackTrace();
         }finally {
             driver.quit();
--- src/main/java/Services/FilmService.java
+++ src/main/java/Services/FilmService.java
@@ -4,6 +4,7 @@ import Pojos.Film;
 import Pojos.Rating;
 import com.mongodb.client.result.UpdateResult;
 import javafx.util.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -18,7 +19,11 @@ public class FilmService {
 
     private final MongoTemplate mongoTemplate;
 
-    public FilmService(MongoTemplate mongoTemplate) {
+    @Autowired
+    private FilmInformationDownloaderService filmInformationDownloaderService;
+
+    public FilmService(MongoTemplate mongoTemplate)
+    {
         this.mongoTemplate = mongoTemplate;
     }
 
@@ -30,34 +35,46 @@ public class FilmService {
         return new Pair<>(true, "Successfully saved: " + updatedFilm.getTitles());
     }
 
-    public List<Film> GetFilmsStartingWith(String filmTitle) {
-        return mongoTemplate.find(
-                new Query(Criteria.where("Title").regex("^"+filmTitle)), Film.class);
-    }
+    public Pair<Boolean, String> DeleteFilm(Film filmToDelete){
 
-    public Pair<Boolean, String> DeleteFilm(String filmToDelete){
-
-        if(!getFilmByFullName(filmToDelete)) {
+        return new Pair<>(false, "deleting not implemented yet!");
+/*
+        if(!CheckIfFilmExists(filmToDelete)) {
             return new Pair<>(false, MessageFormat.format("Film {0} not found", filmToDelete));
         }
 
         mongoTemplate.remove(new Query(Criteria.where("Title").regex(filmToDelete)), Film.class);
         return new Pair<>(true, MessageFormat.format("Film {0} deleted", filmToDelete));
+ */
     }
 
     public Pair<Boolean, String> SaveFilm(Film filmToSave){
 
-        if(getFilmByFullName(filmToSave.getTitles().get(0).getName())) {
+        if(CheckIfFilmExistsByNameAndProductionYear(
+                filmToSave.getTitles().get(0).getName(),
+                filmToSave.getTitles().get(0).getLanguage(),
+                filmToSave.getProductionYear())) {
             System.out.println("There is already film with that title");
             return new Pair<>(false, "There is already film with that title");
         }
+
+        this.filmInformationDownloaderService = new FilmInformationDownloaderService();
+        this.filmInformationDownloaderService.Initialize(
+                filmToSave.getTitles().get(0).getName(),
+                filmToSave.getProductionYear(),
+                filmToSave.getType());
+        var filmRatingsResult = this.filmInformationDownloaderService.GetFilmRatings();
+        System.out.println(filmRatingsResult.getValue());
+        filmToSave.setRatings(filmRatingsResult.getKey());
 
         mongoTemplate.save(filmToSave);
         System.out.println(MessageFormat.format(
                 "Film {0} saved successfully with Id = {1}",
                 filmToSave.getTitles(),
                 filmToSave.getId()));
-        return new Pair<>(true, "Successfully saved: " + filmToSave.getTitles());
+        return new Pair<>(
+                true,
+                "Successfully saved: " + filmToSave.getTitles() + "\n" + filmRatingsResult.getValue());
     }
 
     public Pair<Boolean, String> Update(String key, String value){
@@ -73,8 +90,17 @@ public class FilmService {
         return mongoTemplate.findAll(Film.class);
     }
 
-    private Boolean getFilmByFullName(String filmTitle){
-        return mongoTemplate.exists(
-                new Query(Criteria.where("Title").is(filmTitle)), Film.class);
+    private Boolean CheckIfFilmExistsByNameAndProductionYear(
+            String filmTitle,
+            String filmTitleLanguage,
+            String filmProductionYear){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("Titles").elemMatch(
+                Criteria.where("Name")
+                        .is(filmTitle)
+                        .and("Language")
+                        .is(filmTitleLanguage)));
+        query.addCriteria(Criteria.where("ProductionYear").is(filmProductionYear));
+        return mongoTemplate.exists(query, Film.class);
     }
 }
